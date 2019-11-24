2e017f274b5812f749fa254a1b222f9e3965e48f
Ralitsa Petrina <ralitsa.petrina@gmail.com>
2019-11-24 18:15:42 +0200

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Petrina on 24.11.19.
+//  Copyright © 2019 Ralitsa Petrina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
