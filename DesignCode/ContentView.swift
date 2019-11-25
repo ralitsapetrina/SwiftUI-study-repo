1b8cefc3dbdb26521fafc7af03e92f701e9bdfb8
Ralitsa Petrina <ralitsa.petrina@gmail.com>
2019-11-25 22:36:32 +0200

    Lesson 2 - Layout and Stacks Lesson 3 - Components and Visual Effects

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,25 +10,115 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            VStack {
-                Text("UI Design")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("accent"))
-                    .padding(.top)
-                Text(/*@START_MENU_TOKEN@*/"Certificate"/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(Color.white)
-            }
-            Image("Background")
+        ZStack {
+            
+            TitleView()
+                .blur(radius: 20)
+            
+            CardBottomView()
+                .blur(radius: 20)
+            
+            CardView()
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: 15))
+                .rotation3DEffect(Angle(degrees: 50), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
+                .blendMode(.hardLight)
+            
+            CardView().offset(x: 0, y: -20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(Angle(degrees: 40), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
+                .blendMode(.hardLight)
+            
+            CertificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(Angle(degrees: 30), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
         }
-        .background(Color.black)
-        .cornerRadius(10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("Card Back")
+        }
+        .frame(width: 340, height: 220.0)
+        .background(Color.blue)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("accent"))
+                        .padding(.top)
+                    Text(/*@START_MENU_TOKEN@*/"Certificate"/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("Background")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificate")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Illustration5")
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                .opacity(0.1)
+            Text("This certificate is proof that R. Petrina has achieved the UI design course with aproval from Design+Code instructor")
+                .lineLimit(10)
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y: 600)
     }
 }
