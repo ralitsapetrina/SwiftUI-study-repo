3edb0c5096edd8578267d4aada62148fdccbe59c
Ralitsa Petrina <ralitsa.petrina@gmail.com>
2019-11-29 08:14:25 +0200

    Lesson 5 - Gestures and Events

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var show = false
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -32,9 +33,10 @@ struct ContentView: View {
 //                .rotation3DEffect(Angle(degrees: show ? 50 : 0), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.7))
+                .offset(x: viewState.width, y: viewState.height)
             
             CardView()
-                .background(show ? Color.red : Color("background8"))
+                .background(show ? Color("background5") : Color("background8"))
                 .background(Color.blue)
                 .cornerRadius(10)
                 .shadow(radius: 20)
@@ -44,15 +46,28 @@ struct ContentView: View {
 //                .rotation3DEffect(Angle(degrees: show ? 40 : 0), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.5))
+                .offset(x: viewState.width, y: viewState.height)
             
             CertificateView()
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
                 .rotationEffect(Angle(degrees: show ? 5 : 0))
 //                .rotation3DEffect(Angle(degrees: show ? 30 : 0), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
                 .animation(.interpolatingSpring(mass: 1, stiffness: 100, damping: 10, initialVelocity: 0))
                 .onTapGesture {
                     self.show.toggle()
-            }
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            self.viewState = value.translation
+                            self.show = true
+                        }
+                        .onEnded { value in
+                            self.viewState = .zero
+                            self.show = false
+                        }
+                )
         }
     }
 }
