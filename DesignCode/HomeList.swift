2b69644445cb68d74dc3f03abed466910b21d2d1
Ralitsa Petrina <ralitsa.petrina@gmail.com>
2019-12-09 23:46:39 +0200

    Lesson 10 - Scrolling your Content

---
+++ DesignCode/HomeList.swift
@@ -0,0 +1,49 @@
+//
+//  HomeList.swift
+//  DesignCode
+//
+//  Created by Petrina on 9.12.19.
+//  Copyright © 2019 Ralitsa Petrina. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeList: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0 ..< 3) { item in
+                    CourseView()
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+struct HomeList_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeList()
+    }
+}
+
+struct CourseView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Build and app with SwiftUI")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.leading)
+                .padding(20)
+                .lineLimit(4)
+                .frame(width: 150)
+            Spacer()
+            Image("Illustration1")
+        }
+        .background(Color("background3"))
+        .cornerRadius(30)
+        .frame(width: 246, height: 360)
+        .shadow(color: Color("backgroundShadow3"), radius: 20, x: 0, y: 20)
+    }
+}
